94d8798029f76a65636c7916ea529afb93828527
Real Inspiration <ilerwatson22@gmail.com>
2022-01-30 23:13:54 +0000

    Server to connect to database

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
+require('dotenv').config();
 const mysql = require('mysql2');
-const inquirer = require('inquirer');
-const cTable = require('console.table');
+
+const dbOptions = {
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  port: process.env.PORT,
+};
+
+const db = mysql.createConnection(dbOptions);
+console.log('Connected to the business_db database.');
+
+module.exports = db;
