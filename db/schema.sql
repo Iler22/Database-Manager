afd1fe8925dd451403c75d849029c07db1405c2b
Real Inspiration <ilerwatson22@gmail.com>
2022-01-30 23:35:05 +0000

    Updated schema with index, constraint and delete on cascade

--- db/schema.sql
+++ db/schema.sql
@@ -4,17 +4,23 @@ CREATE DATABASE business_db;
 USE business_db;
 
 CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) 
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL 
 );
 
 CREATE TABLE role (
-id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+id INT AUTO_INCREMENT PRIMARY KEY, 
 title VARCHAR(30) NOT NULL, salary DECIMAL NOT NULL,  
-department_id INT NOT NULL, FOREIGN KEY (department_id) REFERENCES department(id) );
+department_id INT, 
+INDEX department_ind (department_id),
+CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+);
 
 CREATE TABLE employee (
-id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+id INT AUTO_INCREMENT PRIMARY KEY, 
 first_name VARCHAR(30) NOT NULL, last_name VARCHAR(30) NOT NULL,  
-role_id INT NOT NULL, FOREIGN KEY (role_id) REFERENCES role(id),
-manager_id INT NULL, FOREIGN KEY (manager_id) REFERENCES manager(id) );
+role_id INT, 
+INDEX role_ind (role_id),
+CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+manager_id INT 
+);
